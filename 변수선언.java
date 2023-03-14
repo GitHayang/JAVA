468689b79fd16f61ba028f290e66ebef1327d909
GitHayang <127068766+GitHayang@users.noreply.github.com>
2023-03-14 20:00:12 +0900

    변수선언 규칙

---
+++ 변수선언.java
@@ -0,0 +1,22 @@
+
+public class Naming {
+	public static void main(String[] args) {
+
+		int a;
+		int b;
+
+		// 명령어와 예약어를 변수 명으로 쓸 수 없으니 2단어 이상 쓰거나 숫자를 섞는다
+		// 앵간하면 명령어나 예약어와 겹치지 않는다
+
+		// 명령어 선언방법
+		// 대문자로 시작하는건 class와 project만 한다. 나머지는 소문자로 시작
+
+		int worldWar; // camel표기법
+		int world_war; // snake표기법
+
+		// java에서는 예로부터 약속처럼 camel쓴다.
+		// python은 snake를 주로 쓴다.
+		// 물론 예외는 있다
+
+	}
+}
