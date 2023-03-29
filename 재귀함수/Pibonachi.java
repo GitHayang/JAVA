9afa4bce1a36c889d67929744529d57e024cf443
GitHayang <127068766+GitHayang@users.noreply.github.com>
2023-03-29 20:15:14 +0900

    피보나치 수열 완성본

--- 재귀함수/Pibonachi.java
+++ 재귀함수/Pibonachi.java
@@ -1,32 +1,16 @@
-import java.util.Arrays;
 
 public class Pibonachi {
 	public static void main(String[] args) {
-		PibonachiTen(0, 4);
+		System.out.println(PibonachiTen(10));
 	}
 
-	public static void PibonachiTen(int i, int n) { // i=순서,n=목표값
-//		나온 값들을 배열에 저장한 후 더할때 값을 꺼내쓰면 어떨까?
-		int[] arr = new int[n]; // 불러낼 때 초기화 된다.
-		if (i == n) { // 타깃값이 되면 연산을 멈춰라
-			return;
-		}
+	public static int PibonachiTen(int i) {
 		if (i == 0) {
-			arr[i] = 0;
-			System.out.println(Arrays.toString(arr));
-			i++;
-			PibonachiTen(i, n);
+			return 0;
 		} else if (i == 1) {
-			arr[i] = 1;
-			System.out.println(Arrays.toString(arr));
-			i++;
-			PibonachiTen(i, n);
+			return 1;
 		} else {
-			arr[i] = arr[i - 1] + arr[i - 2];
-			System.out.println(Arrays.toString(arr));
-			System.out.println(arr[i]);
-			i++;
-			PibonachiTen(i, n);
+			return PibonachiTen(i - 1) + PibonachiTen(i - 2);
 		}
 	}
 
